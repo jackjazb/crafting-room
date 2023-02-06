5f52bb5f39157fc5cffbe295e5fc33c29ce8ce6a
Jack Jazrawy-Brown <jackjazb@gmail.com>
2023-02-06 17:25:10 +0000

    Implement social icon footer

--- crafting-room-frontend/src/components/Footer.tsx
+++ crafting-room-frontend/src/components/Footer.tsx
@@ -1,7 +1,14 @@
+import {SiInstagram, SiBandcamp, SiYoutube, SiTwitter} from 'react-icons/si'
+
+const SOCIAL_ICON_SIZE = 30;
+
 export function Footer() {
 	return (
 		<div className="footer">
-			<h4>footer</h4>
+			<a href="https://www.instagram.com/crafting_room/?hl=en"><SiInstagram size={SOCIAL_ICON_SIZE - 5}/></a>
+			<a href="https://www.youtube.com/channel/UCY_H8aUrjvI8iHobz5mi9sQ"><SiYoutube size={SOCIAL_ICON_SIZE}/></a>
+			<a href="https://craftingroomrecordings.bandcamp.com/"><SiBandcamp size={SOCIAL_ICON_SIZE}/></a>
+			<a href="https://twitter.com/crafting_room"><SiTwitter size={SOCIAL_ICON_SIZE}/></a>
 		</div>
 	)
 }
