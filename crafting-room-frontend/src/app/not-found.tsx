a5ab995efcd83108783d18a83980c652ead0ac61
Ben <gardinbe@gmail.com>
2024-03-30 11:56:20 +0000

    update 404 page

--- crafting-room-frontend/src/app/not-found.tsx
+++ crafting-room-frontend/src/app/not-found.tsx
@@ -2,11 +2,16 @@ import type { NextPage } from 'next';
 
 const NotFoundPage: NextPage = () => {
     return (
-        <div className='container'>
-            <h1>
-                404 - Page Not Found
-            </h1>
-        </div>
+        <main>
+            <section className='container'>
+                <h1>
+                    404
+                </h1>
+                <span>
+                    Sorry, the requested content could not be found.
+                </span>
+            </section>
+        </main>
     );
 };
 
