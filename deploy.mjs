9075dd342727b63fa1c1c8500b7796b370b4bf60
Jack Jazrawy-Brown <jackjazb@gmail.com>
2025-04-24 22:58:08 +0100

    update config

--- deploy.mjs
+++ deploy.mjs
@@ -1,6 +1,32 @@
-import { $ } from "zx";
+import { exit } from "process";
+import { $, chalk, echo } from "zx";
 
-const target = "jack@localhost:/home/";
+function print(...args) {
+    echo(chalk.green(args));
+}
+
+// Remote user to deploy as.
+const user = "root";
+
+// Remote host to deploy to.
+const host = "craftingroomrecordings.co.uk";
+
+// The directory on `host` to deploy to.
+const dir = "/root/beta/";
+
+// The local dir to deploy, containing a pm2 ecosystem file.
 const artifact = "dist";
 
-$`rsync -r ${target}: ${artifact}`;
+const machine = `${user}@${host}`;
+const target = `${machine}:${dir}`;
+
+print(`rsync -avu --mkpath --delete-before ${artifact}/ ${target}`);
+await $`rsync -avu --mkpath --delete-before ${artifact}/ ${target}`;
+print(`synced '${artifact}' to ${host}`);
+exit();
+// console.log(`ssh ${machine} "cd ${dir}; pm2 start ecosystem.config.js"`);
+
+// await $`ssh ${machine} "sudo apt install node"`;
+// await $`ssh ${machine} "npm i -g pm2"`;
+
+// await $`ssh ${machine} "cd ${dir}; pm2 start ecosystem.config.js"`;
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -11,6 +11,12 @@ module.exports = {
             cwd: "./site",
             script: "node",
             args: ["build"],
+            env: {
+                PORT: "3001",
+                STRAPI_HOST: "https://api.craftingroomrecordings.co.uk",
+                PUBLIC_MEDIA_HOST: "https://api.craftingroomrecordings.co.uk",
+                ORIGIN: "https://beta.craftingroomrecordings.co.uk",
+            },
         },
     ],
 };
